93ef86f92901d37daa77736edc39d5098309113c
Musfiqur Rahman <musfiqurrahman.lab08@gmail.com>
2022-09-24 22:35:38 -0400

    adding automatic_fork.py and read_file_content.py

--- src/automatic_fork.py
+++ src/automatic_fork.py
@@ -1,1 +1,15 @@
 # this is a python file
+from github import Github
+
+
+g = Github("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")
+
+
+github_user = g.get_user()
+
+repo = g.get_repo('datarobot/pic2vec')
+
+myfork = github_user.create_fork(repo)
+
+
+print(myfork)
---
+++ src/read_file_content.py
@@ -0,0 +1,12 @@
+from github import Github
+
+
+g = Github("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")
+
+
+github_user = g.get_user()
+
+repo = github_user.get_repo('pic2vec')
+file_content = repo.get_contents('.travis.yml')
+
+print(file_content.decoded_content.decode())
