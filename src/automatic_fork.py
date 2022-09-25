cf8311d9e3eb4354638ccdb65bd51f1541c347b0
Musfiqur Rahman <musfiqurrahman.lab08@gmail.com>
2022-09-25 01:20:07 -0400

    adding trail and error code

--- src/automatic_fork.py
+++ src/automatic_fork.py
@@ -7,7 +7,7 @@ g = Github("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")
 
 github_user = g.get_user()
 
-repo = g.get_repo('datarobot/pic2vec')
+repo = g.get_repo('marl/openl3')
 
 myfork = github_user.create_fork(repo)
 
