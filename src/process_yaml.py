dda786b74952372abccf355f534c340546eaf0df
Musfiqur Rahman <musfiqurrahman.lab08@gmail.com>
2023-01-09 00:44:08 -0500

    moving methods inside the class

--- src/process_yaml.py
+++ src/process_yaml.py
@@ -4,19 +4,6 @@ from anytree.dotexport import DotExporter
 from graphviz import Source, render
 
 
-def save_tree_image(tree, file_name):
-    DotExporter(tree).to_dotfile(file_name)
-    Source.from_file(file_name)
-    render('dot', 'png', file_name)
-
-
-def print_tree(tree, ascii_style=False):
-    for pre, fill, node in RenderTree(tree):
-        print("%s%s" % (pre, node.name))
-    if ascii_style:
-        print(RenderTree(tree, style=AsciiStyle()).by_attr())
-
-
 class ProcessYAML:
     def __init__(self, yaml_stream):
         try:
@@ -38,6 +25,19 @@ class ProcessYAML:
             else:
                 Node(value, parent=node)
         return root
+
+    @staticmethod
+    def save_tree_image(tree, file_name):
+        DotExporter(tree).to_dotfile(file_name)
+        Source.from_file(file_name)
+        render('dot', 'png', file_name)
+
+    @staticmethod
+    def print_tree(tree, ascii_style=False):
+        for pre, fill, node in RenderTree(tree):
+            print("%s%s" % (pre, node.name))
+        if ascii_style:
+            print(RenderTree(tree, style=AsciiStyle()).by_attr())
 
     def __del__(self):
         print("Collecting garbage...Done.")
